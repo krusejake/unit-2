28bc6bc1640d350e2e9cdffb1101d417c9df4d54
krusejake <47227749+krusejake@users.noreply.github.com>
2022-02-23 14:03:18 -0600

    Finished and commented on functions for leaflet tutorials

---
+++ js/quickstartTutorial.js
@@ -0,0 +1,63 @@
+// // Leaflet tutorial: https://leafletjs.com/examples/quick-start/
+
+console.log('start of js file')
+//initialize map with center and zoom
+var map = L.map('mapid').setView([51.505, -0.09], 13);
+
+// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox/streets-v11',
+//     tileSize: 512,
+//     zoomOffset: -1,
+//     accessToken: 'your.mapbox.access.token'
+// }).addTo(map);
+
+// // Example 1.1 line 5...add tile layer..pulls from openstreetmap
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+}).addTo(map);
+//adds marker
+var marker = L.marker([51.5, -0.09]).addTo(map);
+//specify circler and add to map
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+//specify polygon and add to map
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
+
+// add popups to the three object added to map, and to standalone location
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+var popup = L.popup()
+    .setLatLng([51.513, -0.09])
+    .setContent("I am a standalone popup.")
+    .openOn(map);
+
+    function onMapClick(e) {
+        alert("You clicked the map at " + e.latlng);
+    }
+    // have map listen for 'lick'...if click, give popup at location with message
+    map.on('click', onMapClick);    
+
+
+var popup = L.popup();
+// duplicate code
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
+
