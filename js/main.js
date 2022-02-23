56a2aa1bb6b9d2d9cefedd3633635268a8ef5f9a
krusejake <47227749+krusejake@users.noreply.github.com>
2022-02-23 17:31:35 -0600

    Created final map

--- js/main.js
+++ js/main.js
@@ -1,1 +1,59 @@
 // Add all scripts to the JS folder
+var map = L.map('mapid').setView([25, 25], 5);
+    //add OSM base tilelayer
+// L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+// }).addTo(map);
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+    //call getData function
+getData();
+// };
+
+var popup = L.popup()
+    .setLatLng([30, 25])
+    .setContent("Military spending (in millions of USD) by year across Northern Africa and the Middle East. Points represent country capital cities.")
+    .openOn(map);
+
+//added at Example 2.3 line 20...function to attach popups to each mapped feature
+function onEachFeature(feature, layer) {
+    //no property named popupContent; instead, create html string with all properties
+    var popupContent = "";
+    if (feature.properties) {
+        //loop to add feature property names and values to html string
+        for (var property in feature.properties){
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    };
+};
+
+//function to retrieve the data and place it on the map
+function getData(){
+    //load the data
+    fetch("data/militaryspending.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){            
+            //create marker options
+            var geojsonMarkerOptions = {
+                radius: 8,
+                fillColor: "#ff7800",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(json, {
+                onEachFeature: onEachFeature,
+                pointToLayer: function (feature, latlng){
+                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                }
+            }).addTo(map);
+        });              
+};
