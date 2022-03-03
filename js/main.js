ba8276f872d27664e01f7a0f8042eaafad88923f
krusejake <47227749+krusejake@users.noreply.github.com>
2022-03-02 19:33:58 -0600

    Final updates for lab

--- js/main.js
+++ js/main.js
@@ -207,11 +207,12 @@ function calcPropRadius(attValue) {
 };
 
 //function to convert markers to circle markers
-function pointToLayer(feature, latlng){
+function pointToLayer(feature, latlng,attributes){
     // console.log('started pointToLayer')
     //Determine which attribute to visualize with proportional symbols
-    var attribute = "2020";
-
+    // var attribute = "2020";
+    var attribute = attributes[0];
+    console.log('attributes in pointToLayer: '+attributes)
     //create marker options
     var options = {
         fillColor: "#ff7800",
@@ -238,19 +239,20 @@ function pointToLayer(feature, latlng){
     //add formatted attribute to popup content string
     // var year = attribute.split("_")[1];
 
-    // popupContent += "<p><b><Military spending> in " + attribute + ":</b> " + feature.properties[attribute] + " millions of USD</p>";
-    console.log(popupContent)
+    popupContent += "<p><b>Military spending in " + attribute + ":</b> " + feature.properties[attribute] + " millions,  USD</p>";
+    // console.log(popupContent)
     //bind the popup to the circle marker
     layer.bindPopup(popupContent, {
         offset: new L.Point(0,-options.radius) 
     });
-    console.log('bound the popup to the circle maker')
+    // console.log('bound the popup to the circle maker')
     //return the circle marker to the L.geoJson pointToLayer option
     return layer;
 };
 
 //Step 1: Create new sequence controls
-function createSequenceControls(){
+function createSequenceControls(attributes){
+    console.log('entered createSequenceControls')
     //create range input element (slider)
     var slider = "<input class='range-slider' type='range'></input>";
     document.querySelector("#panel").insertAdjacentHTML('beforeend',slider);
@@ -260,40 +262,114 @@ function createSequenceControls(){
     document.querySelector(".range-slider").value = 0;
     document.querySelector(".range-slider").step = 1;
     //below Example 3.6...add step buttons
-    // document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="reverse">Reverse</button>');
-    // document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="forward">Forward</button>');
+    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="reverse">Reverse</button>');
+    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="forward">Forward</button>');
+    //replace button content with images
+    document.querySelector('#reverse').insertAdjacentHTML('beforeend',"<img src='img/reverse.png'>");
+    document.querySelector('#forward').insertAdjacentHTML('beforeend',"<img src='img/forward.png'>");
+    //Below Example 3.6 in createSequenceControls()
+    //Step 5: click listener for buttons
+    document.querySelectorAll('.step').forEach(function(step){
+        step.addEventListener("click", function(){
+            //sequence
+        })
+    })
+
+    //Step 5: input listener for slider
+    document.querySelector('.range-slider').addEventListener('input', function(){            
+        //sequence
+    });
+    //Step 5: input listener for slider
+    document.querySelector('.range-slider').addEventListener('input', function(){
+        //Step 6: get the new index value
+        var index = this.value;
+        console.log(index)
+    });
+    document.querySelectorAll('.step').forEach(function(step){
+        step.addEventListener("click", function(){
+            var index = document.querySelector('.range-slider').value;
+
+            //Step 6: increment or decrement depending on button clicked
+            if (step.id == 'forward'){
+                index++;
+                //Step 7: if past the last attribute, wrap around to first attribute
+                index = index > 10 ? 0 : index;
+                //Called in both step button and slider event listener handlers
+                //Step 9: pass new attribute to update symbols
+                updatePropSymbols(attributes[index]);
+            } else if (step.id == 'reverse'){
+                index--;
+                //Step 7: if past the first attribute, wrap around to last attribute
+                index = index < 0 ? 10 : index;
+                //Called in both step button and slider event listener handlers
+                //Step 9: pass new attribute to update symbols
+                console.log('got to updatePropSymbols')
+                updatePropSymbols(attributes[index]);
+                console.log('finished updatePropSymbols')
+
+            };
+
+            //Step 8: update slider
+            document.querySelector('.range-slider').value = index;
+        })
+    })
     console.log('created sequence control');
 };
+//Step 10: Resize proportional symbols according to new attribute values
+function updatePropSymbols(attribute){
+    map.eachLayer(function(layer){
+        console.log('will updatePopSymbols...')
+        if (layer.feature && layer.feature.properties[attribute]){
+            //access feature properties
+            var props = layer.feature.properties;
 
-// //Step 3: Add circle markers for point features to the map
-function createPropSymbols(data){
+            //update each feature's radius based on new attribute values
+            var radius = calcPropRadius(props[attribute]);
+            layer.setRadius(radius);
 
-    //Step 4: Determine which attribute to visualize with proportional symbols
-    var attribute = "2020";
+            //add city to popup content string
+            var popupContent = "<p><b>City:</b> " + layer.feature.properties.Country + "</p>";
+  
+            popupContent += "<p><b>Military spending in " + attribute + ":</b> " + layer.feature.properties[attribute] + " millions,  USD</p>";
 
-    //create marker options
-    var geojsonMarkerOptions = {
-        fillColor: "#ff7800",
-        color: "#fff",
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.8,
-        radius: 8
-    };
-
-    L.geoJson(data, {
-        pointToLayer: function (feature, latlng) {
-            //Step 5: For each feature, determine its value for the selected attribute
-            var attValue = Number(feature.properties[attribute]);
-
-            //Step 6: Give each feature's circle marker a radius based on its attribute value
-            geojsonMarkerOptions.radius = calcPropRadius(attValue);
-
-            //create circle markers
-            return L.circleMarker(latlng, geojsonMarkerOptions);
-        }
-    }).addTo(map);
+            //update popup content            
+            popup = layer.getPopup();            
+            popup.setContent(popupContent).update();
+            console.log('made it to end of update')
+        };
+        
+    });
 };
+
+// // //Step 3: Add circle markers for point features to the map
+// function createPropSymbols(data){
+
+//     //Step 4: Determine which attribute to visualize with proportional symbols
+//     var attribute = "2020";
+
+//     //create marker options
+//     var geojsonMarkerOptions = {
+//         fillColor: "#ff7800",
+//         color: "#fff",
+//         weight: 1,
+//         opacity: 1,
+//         fillOpacity: 0.8,
+//         radius: 8
+//     };
+
+//     L.geoJson(data, {
+//         pointToLayer: function (feature, latlng) {
+//             //Step 5: For each feature, determine its value for the selected attribute
+//             var attValue = Number(feature.properties[attribute]);
+
+//             //Step 6: Give each feature's circle marker a radius based on its attribute value
+//             geojsonMarkerOptions.radius = calcPropRadius(attValue);
+
+//             //create circle markers
+//             return L.circleMarker(latlng, geojsonMarkerOptions);
+//         }
+//     }).addTo(map);
+// };
 // //Add circle markers for point features to the map
 // function createPropSymbols(data, map){
 //     //create a Leaflet GeoJSON layer and add it to the map
@@ -301,6 +377,38 @@ function createPropSymbols(data){
 //         pointToLayer: pointToLayer
 //     }).addTo(map);
 // };
+
+function createPropSymbols(data, attributes){
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng){
+            return pointToLayer(feature, latlng, attributes);
+        }
+    }).addTo(map);
+};
+
+//Above Example 3.10...Step 3: build an attributes array from the data
+function processData(data){
+    //empty array to hold attributes
+    var attributes = [];
+
+    //properties of the first feature in the dataset
+    var properties = data.features[0].properties;
+    //push each attribute name into attributes array
+    for (var attribute in properties){
+        //only take attributes with population values
+        // if (attribute.indexOf("Pop") > -1){
+        if ((attribute !='Latitude')&(attribute !='Longitude')&(attribute !='Country')) {
+            attributes.push(attribute);
+            // console.log(attributes)
+        };
+    };
+
+    //check result
+    console.log(attributes);
+
+    return attributes;
+};
 
 //Step 2: Import GeoJSON data
 function getData(){
@@ -311,12 +419,16 @@ function getData(){
             return response.json();
         })
         .then(function(json){
-            //calculate minimum data value
+            var attributes = processData(json);
             minValue = calculateMinValue(json);
-            //call function to create proportional symbols
-            createPropSymbols(json);
-            createSequenceControls();
-            console.log('ran creatPropSymbols')
+            createPropSymbols(json, attributes);
+            createSequenceControls(attributes);
+            //calculate minimum data value
+            // minValue = calculateMinValue(json);
+            // //call function to create proportional symbols
+            // createPropSymbols(json);
+            // createSequenceControls();
+            console.log('ran createPropSymbols')
         })
 };
 
