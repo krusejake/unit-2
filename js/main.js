1edca96ab28b665c5d7522e11a3d37f286f7a5a4
krusejake <47227749+krusejake@users.noreply.github.com>
2022-03-06 19:47:00 -0600

    OOP refactoring

--- js/main.js
+++ js/main.js
@@ -1,168 +1,28 @@
-// // Add all scripts to the JS folder
-// var minValue;
-// var map = L.map('mapid').setView([25, 25], 5);
-//     //add OSM base tilelayer
-// L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-// 	maxZoom: 20,
-// 	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-// }).addTo(map);
-// var minValue;
-
-//     //call getData function
-// getData();
-// // };
-
-// var popup = L.popup()
-//     .setLatLng([30, 25])
-//     .setContent("Military spending (in millions of USD) by year across Northern Africa and the Middle East. Points represent country capital cities.")
-//     .openOn(map);
-
-// //added at Example 2.3 line 20...function to attach popups to each mapped feature
-// function onEachFeature(feature, layer) {
-//     //no property named popupContent; instead, create html string with all properties
-//     var popupContent = "";
-//     if (feature.properties) {
-//         //loop to add feature property names and values to html string
-//         for (var property in feature.properties){
-//             popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
-//         }
-//         layer.bindPopup(popupContent);
-//     };
-// };
-
-// // //function to retrieve the data and place it on the map
-// // function getData(){
-// //     //load the data
-// //     fetch("data/militaryspending.geojson")
-// //         .then(function(response){
-// //             return response.json();
-// //         })
-// //         .then(function(json){            
-// //             //create marker options
-// //             var geojsonMarkerOptions = {
-// //                 radius: 8,
-// //                 fillColor: "#ff7800",
-// //                 color: "#000",
-// //                 weight: 1,
-// //                 opacity: 1,
-// //                 fillOpacity: 0.8
-// //             };
-// //             //create a Leaflet GeoJSON layer and add it to the map
-// //             L.geoJson(json, {
-// //                 onEachFeature: onEachFeature,
-// //                 pointToLayer: function (feature, latlng){
-// //                     return L.circleMarker(latlng, geojsonMarkerOptions);
-// //                 }
-// //             }).addTo(map);
-// //         });              
-// // };
-
-// //Step 3: Add circle markers for point features to the map
-// function createPropSymbols(data){
-//     //Step 4. Determine the attribute for scaling the proportional symbols
-//     var attribute = 2020;
-//     //create marker options
-//     var geojsonMarkerOptions = {
-//         radius: 8,
-//         fillColor: "#ff7800",
-//         color: "#000",
-//         weight: 1,
-//         opacity: 1,
-//         fillOpacity: 0.8
-//     };
-
-//     //create a Leaflet GeoJSON layer and add it to the map
-//     L.geoJson(data, {
-//         pointToLayer: function (feature, latlng) {
-//             //Step 5: For each feature, determine its value for the selected attribute
-//             var attValue = Number(feature.properties[attribute]);
-//             // console.log('attValue:'+feature.properties["Country"])
-//             console.log('attValue for '+feature.properties["Country"]+": "+attValue)
-//             //examine the attribute value to check that it is correct
-//             console.log(feature.properties, attValue);
-//              //Step 6: Give each feature's circle marker a radius based on its attribute value
-//             geojsonMarkerOptions.radius = calcPropRadius(attValue);
-//             //create circle markers
-//             return L.circleMarker(latlng, geojsonMarkerOptions);
-//         }
-//     }).addTo(map);
-// };
-
-// //Step 2: Import GeoJSON data
-// function getData(){
-//     //load the data
-//     fetch("data/militaryspending.geojson")
-//         .then(function(response){
-//             return response.json();
-//         })
-//         .then(function(json){
-//             calculateMinValue(json);
-//             console.log('minValue in createPropSymbols: '+minValue);
-//             createPropSymbols(json);
-//             console.log('Ran createPropSymbols')
-//         })
-// };
-
-// function calculateMinValue(data){
-//     //create empty array to store all data values
-//     var allValues = [];
-//     //loop through each city
-//     for(var city of data.features){
-//         //loop through each year
-//         for(var year = 1985; year <= 2015; year+=5){
-//               //get population for current year
-//               var value = city.properties["Pop_"+ String(year)];
-//               //add value to array
-//               allValues.push(value);
-//         }
-//     }
-//     //get minimum value of our array
-//     var minValue = Math.min(...allValues)
-//     console.log('Made it to the end of calculateMinValue...value of minValue = '+minValue)
-//     return minValue;
-// }
-
-
-// //calculate the radius of each proportional symbol
-// function calcPropRadius(attValue) {
-//     //constant factor adjusts symbol sizes evenly
-//     var minRadius = 5;
-//     //Flannery Apperance Compensation formula
-//     var middleVal = attValue/minValue
-//     var radius = 1.0083 * Math.pow(middleVal,0.5715)*minRadius
-//     console.log('radius: '+radius)
-//     console.log('attValue: '+attValue)
-//     console.log('minRadius: '+minRadius)
-//     console.log('minValue: '+minValue)
-//     console.log('middleVal: '+middleVal)
-//     return radius;
-// };
-
-// // document.addEventListener('DOMContentLoaded',createMap)
-
-
-//______alt code________________________________________-
 //declare map variable globally so all functions have access
 var map;
 var minValue;
 
+//Example 1.2 line 1...PopupContent constructor function
+function PopupContent(properties, attribute){
+    this.properties = properties;
+    this.year = attribute
+    this.spending = this.properties[attribute];
+    this.formatted = "<p><b>County:</b> " + this.properties['Country'] + "</p><p><b>Miltiary spending in " + this.year + ":</b> " + this.spending + " million</p>";
+};
+
 //step 1 create map
 function createMap(){
-
     //create the map
     map = L.map('mapid', {
         center: [25, 25],
         zoom: 4
     });
 
-    //add OSM base tilelayer
-    // L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    //add base tilelayer
     L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
 	maxZoom: 20,
 	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
     }).addTo(map);
-
     //call getData function
     getData(map);
 };
@@ -173,9 +33,8 @@ function calculateMinValue(data){
     //loop through each country
     for(var country of data.features){
         //loop through each year
-        // console.log(country.properties['Country']+': ')
         for(var year = 2010; year <= 2020; year+=1){
-              //get population for current year
+              //get spending for current year
               var value = country.properties[year];
             //   console.log(year+': '+value)
               //add value to array
@@ -186,10 +45,6 @@ function calculateMinValue(data){
     }
     //get minimum value of our array
     var minValue = Math.min(...allValues)
-    // console.log('minValue = '+minValue)
-    // if (minValue == 0) {
-    //     minValue = 1000
-    // } 
     return minValue;
 }
 
@@ -197,20 +52,14 @@ function calculateMinValue(data){
 function calcPropRadius(attValue) {
     //constant factor adjusts symbol sizes evenly
     var minRadius = 5;
-    // console.log('minRadius = '+minRadius)
     //Flannery Apperance Compensation formula
     var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
-    // console.log('attValue = '+attValue)
-    // console.log('minValue = '+minValue)
-    // console.log('radius = '+radius)
     return radius;
 };
 
 //function to convert markers to circle markers
-function pointToLayer(feature, latlng,attributes){
-    // console.log('started pointToLayer')
+function pointToLayer(feature,latlng,attributes){
     //Determine which attribute to visualize with proportional symbols
-    // var attribute = "2020";
     var attribute = attributes[0];
     console.log('attributes in pointToLayer: '+attributes)
     //create marker options
@@ -224,29 +73,15 @@ function pointToLayer(feature, latlng,attributes){
 
     //For each feature, determine its value for the selected attribute
     var attValue = Number(feature.properties[attribute]);
-    // console.log('set attValues: '+attValue)
     //Give each feature's circle marker a radius based on its attribute value
     options.radius = calcPropRadius(attValue);
-
     //create circle marker layer
     var layer = L.circleMarker(latlng, options);
-
-    //build popup content string
-    // var popupContent = "<p><b>City:</b> " + feature.properties.City + "</p><p><b>" + attribute + ":</b> " + feature.properties[attribute] + "</p>";
-    //build popup content string starting with city...Example 2.1 line 24
-    var popupContent = "<p><b>City:</b> " + feature.properties.Country + "</p>";
-    // console.log(popupContent)
-    //add formatted attribute to popup content string
-    // var year = attribute.split("_")[1];
-
-    popupContent += "<p><b>Military spending in " + attribute + ":</b> " + feature.properties[attribute] + " millions,  USD</p>";
-    // console.log(popupContent)
+    var popup = new PopupContent(feature.properties, attribute)
     //bind the popup to the circle marker
-    layer.bindPopup(popupContent, {
+    layer.bindPopup(popup.formatted, {
         offset: new L.Point(0,-options.radius) 
     });
-    // console.log('bound the popup to the circle maker')
-    //return the circle marker to the L.geoJson pointToLayer option
     return layer;
 };
 
@@ -276,10 +111,6 @@ function createSequenceControls(attributes){
     })
 
     //Step 5: input listener for slider
-    document.querySelector('.range-slider').addEventListener('input', function(){            
-        //sequence
-    });
-    //Step 5: input listener for slider
     document.querySelector('.range-slider').addEventListener('input', function(){
         //Step 6: get the new index value
         var index = this.value;
@@ -288,7 +119,6 @@ function createSequenceControls(attributes){
     document.querySelectorAll('.step').forEach(function(step){
         step.addEventListener("click", function(){
             var index = document.querySelector('.range-slider').value;
-
             //Step 6: increment or decrement depending on button clicked
             if (step.id == 'forward'){
                 index++;
@@ -303,80 +133,32 @@ function createSequenceControls(attributes){
                 index = index < 0 ? 10 : index;
                 //Called in both step button and slider event listener handlers
                 //Step 9: pass new attribute to update symbols
-                console.log('got to updatePropSymbols')
                 updatePropSymbols(attributes[index]);
-                console.log('finished updatePropSymbols')
-
             };
-
             //Step 8: update slider
             document.querySelector('.range-slider').value = index;
         })
     })
-    console.log('created sequence control');
 };
 //Step 10: Resize proportional symbols according to new attribute values
 function updatePropSymbols(attribute){
     map.eachLayer(function(layer){
-        console.log('will updatePopSymbols...')
+        // console.log('will updatePopSymbols...')
         if (layer.feature && layer.feature.properties[attribute]){
             //access feature properties
             var props = layer.feature.properties;
-
             //update each feature's radius based on new attribute values
             var radius = calcPropRadius(props[attribute]);
             layer.setRadius(radius);
-
-            //add city to popup content string
-            var popupContent = "<p><b>City:</b> " + layer.feature.properties.Country + "</p>";
-  
-            popupContent += "<p><b>Military spending in " + attribute + ":</b> " + layer.feature.properties[attribute] + " millions,  USD</p>";
-
+            // console.log('props: '+props['Country'])
+            var popupContent = new PopupContent(props, attribute)
             //update popup content            
             popup = layer.getPopup();            
-            popup.setContent(popupContent).update();
-            console.log('made it to end of update')
+            popup.setContent(popupContent.formatted).update();
         };
         
     });
 };
-
-// // //Step 3: Add circle markers for point features to the map
-// function createPropSymbols(data){
-
-//     //Step 4: Determine which attribute to visualize with proportional symbols
-//     var attribute = "2020";
-
-//     //create marker options
-//     var geojsonMarkerOptions = {
-//         fillColor: "#ff7800",
-//         color: "#fff",
-//         weight: 1,
-//         opacity: 1,
-//         fillOpacity: 0.8,
-//         radius: 8
-//     };
-
-//     L.geoJson(data, {
-//         pointToLayer: function (feature, latlng) {
-//             //Step 5: For each feature, determine its value for the selected attribute
-//             var attValue = Number(feature.properties[attribute]);
-
-//             //Step 6: Give each feature's circle marker a radius based on its attribute value
-//             geojsonMarkerOptions.radius = calcPropRadius(attValue);
-
-//             //create circle markers
-//             return L.circleMarker(latlng, geojsonMarkerOptions);
-//         }
-//     }).addTo(map);
-// };
-// //Add circle markers for point features to the map
-// function createPropSymbols(data, map){
-//     //create a Leaflet GeoJSON layer and add it to the map
-//     L.geoJson(data, {
-//         pointToLayer: pointToLayer
-//     }).addTo(map);
-// };
 
 function createPropSymbols(data, attributes){
     //create a Leaflet GeoJSON layer and add it to the map
@@ -391,29 +173,21 @@ function createPropSymbols(data, attributes){
 function processData(data){
     //empty array to hold attributes
     var attributes = [];
-
     //properties of the first feature in the dataset
     var properties = data.features[0].properties;
     //push each attribute name into attributes array
     for (var attribute in properties){
         //only take attributes with population values
-        // if (attribute.indexOf("Pop") > -1){
         if ((attribute !='Latitude')&(attribute !='Longitude')&(attribute !='Country')) {
             attributes.push(attribute);
-            // console.log(attributes)
         };
     };
-
-    //check result
-    console.log(attributes);
-
     return attributes;
 };
 
 //Step 2: Import GeoJSON data
 function getData(){
     //load the data
-    // fetch("data/MegaCities.geojson")
     fetch("data/militaryspending.geojson")
         .then(function(response){
             return response.json();
@@ -423,12 +197,6 @@ function getData(){
             minValue = calculateMinValue(json);
             createPropSymbols(json, attributes);
             createSequenceControls(attributes);
-            //calculate minimum data value
-            // minValue = calculateMinValue(json);
-            // //call function to create proportional symbols
-            // createPropSymbols(json);
-            // createSequenceControls();
-            console.log('ran createPropSymbols')
         })
 };
 
